9c0d644303f1287653e5ffa0e3b18dc7abd9b0e0
Johnathan Martell <Martell-J@users.noreply.github.com>
2018-05-15 23:45:26 -0400

    Client/Server Model Separation.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,59 @@
-// server/index.js
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
 
-const app = require("./app");
+const app = express();
+
+// parse body messages for post route
+const bodyParser = require("body-parser");
+
+const PRODUCTION_PORT = 8080;
+const DEVELOPMENT_PORT = 3001;
+
+const { env } = process;
+
+const { NODE_ENV } = env;
+
+const PORT = NODE_ENV === "production" ? PRODUCTION_PORT : DEVELOPMENT_PORT;
+
+console.log(NODE_ENV)
+
+app.use(bodyParser.json());
+
+// We could conditionally catch errors if we're in dev vs. prod, but in this
+// case it doesn't really matter. We should be smart enough to handle errors
+// with expected cases
+process.on("unhandledRejection", (err) => {
+
+  // Actually throw stack-traces for unhandled rejections (mainly for promise debugging)
+  throw err;
+
+});
+
+// Inject the db routes via express router, pass app to it (sequelize is tied in)
+const dbRoutes = require("./routes/db.js");
+app.use("/api", dbRoutes);
+
+// Setup logger
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+
+// If we're in production, serve the files and static assets.
+if (NODE_ENV === "production") {
+
+  // Serve static assets
+  app.use(express.static(path.resolve(__dirname, "..", "build")));
+
+  app.get("*", (req, res) => {
+
+    // Render base index.html page in every route
+    res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
+
+  });
+
+}
+
+app.listen(PORT, () => {
+
+  console.log(`App listening on port ${PORT}!`);
+
+});
