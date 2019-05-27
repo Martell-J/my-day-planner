b886b5c4bf2889fdf5488af8433d798d2bc17f88
null <Martell-J@users.noreply.github.com>
2019-05-26 23:23:22 -0400

    Forcibly reroute requests using http-proxy-middleware

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const morgan = require("morgan");
 const path = require("path");
 
 const app = express();
-
+const config = require("./config/config.json");
+const api_key = process.env.API_KEY || config.api_key;
+const api_url = process.env.API_URL || config.api_url;
+const httpProxy = require("http-proxy-middleware");
 // parse body messages for post route
 const bodyParser = require("body-parser");
 
@@ -18,15 +21,15 @@ const PORT = (NODE_ENV === "production" ? PRODUCTION_PORT : DEVELOPMENT_PORT);
 
 const API = require("./helpers/api.js");
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 // We could conditionally catch errors if we're in dev vs. prod, but in this
 // case it doesn't really matter. We should be smart enough to handle errors
 // with expected cases
 process.on("unhandledRejection", (err) => {
 
-  // Actually throw stack-traces for unhandled rejections (mainly for promise debugging)
-  throw err;
+ // Actually throw stack-traces for unhandled rejections (mainly for promise debugging)
+ throw err;
 
 });
 
@@ -37,32 +40,49 @@ const serverRequestMethods = [ "get", "post", "put", "delete" ];
 
 // Generify each server-handled request method and inject the pertinent type
 // into express Router
+/*
 serverRequestMethods.forEach((srm) => {
 
-  app[srm]("/api/*", (req, res) => API[srm](req, res));
+ app.use("/api", (req, res) => API[srm](req, res));
 
-});
+});*/
+
+app.use('/api', httpProxy({
+  "logLevel" : 'debug',
+  "target" : api_url,
+  "changeOrigin" : true,
+  "secure" : false,
+  "xfwd" : true,
+  "pathRewrite" : {'^/api' : ''},
+  "onProxyReq" : function (proxyReq, req, res) {
+  if (proxyReq.getHeader('origin')) {
+    proxyReq.setHeader('origin', api_url);
+    }
+    proxyReq.setHeader('api_key', api_key);
+    }
+  })
+);
 
 // Setup logger
-app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+// app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
 // If we're in production, serve the files and static assets.
 if (NODE_ENV === "production") {
 
-  // Serve static assets
-  app.use(express.static(path.resolve(__dirname, "..", "build")));
+ // Serve static assets
+ app.use(express.static(path.resolve(__dirname, "..", "build")));
 
-  app.get("*", (req, res) => {
+ app.get("*", (req, res) => {
 
-    // Render base index.html page in every route
-    res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
+ // Render base index.html page in every route
+ res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
 
-  });
+ });
 
 }
 
 app.listen(PORT, () => {
 
-  console.log(`App listening on port ${PORT}!`);
+ console.log(`App listening on port ${PORT}!`);
 
 });
